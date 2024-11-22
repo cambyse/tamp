0cd18cd62d9d80c453e7a980cb9118cce303ef4a
Camille Phiquepal <camille.phiquepal@gmail.com>
2024-11-22 18:57:30 +0100

    move command line arg handling to utility

---
+++ libs/POMTP_controllers/utility.h
@@ -0,0 +1,35 @@
+/*  ------------------------------------------------------------------
+    Copyright 2016 Camille Phiquepal
+    email: camille.phiquepal@gmail.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at
+    your option) any later version. This program is distributed without
+    any warranty. See the GNU General Public License for more details.
+    You should have received a COPYING file of the full GNU General Public
+    License along with this program. If not, see
+    <http://www.gnu.org/licenses/>
+    --------------------------------------------------------------  */
+
+#include <map>
+
+#pragma once
+
+std::map<std::string, std::string> parseArguments(int argc, char* argv[]) {
+    std::map<std::string, std::string> args;
+    for (int i = 1; i < argc; ++i) {
+        std::string key = argv[i];
+        if (key.rfind("-", 0) == 0 && i + 1 < argc) { // Starts with "--" and has a value
+            args[key] = argv[++i];
+        } else {
+            args[key] = "";
+        }
+    }
+    return args;
+}
+
+std::string value_or(const std::map<std::string, std::string>& m, const std::string& key, const std::string& default_value) {
+    auto it = m.find(key);
+    return it != m.end() ? it->second : default_value;
+}
