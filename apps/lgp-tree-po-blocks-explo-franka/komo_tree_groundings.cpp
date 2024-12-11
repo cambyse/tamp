d659505a1cab1e5bf071478d236f3da42446c824
Camille Phiquepal <camille.phiquepal@gmail.com>
2024-12-11 13:09:08 +0100

    explo franka parameterizable via command line

--- apps/lgp-tree-po-blocks-explo-franka/komo_tree_groundings.cpp
+++ apps/lgp-tree-po-blocks-explo-franka/komo_tree_groundings.cpp
@@ -10,6 +10,7 @@
 #include <Kin/TM_AboveBox.h>
 #include <Kin/TM_qLimits.h>
 
+#include <kin_equality_task.h>
 #include <observation_tasks.h>
 #include <axis_alignment.h>
 
@@ -17,9 +18,9 @@ using namespace rai;
 using W = mp::KomoWrapper;
 
 constexpr bool activateObjectives{true};
+constexpr double JointBounds_scale{1.0e2};
 constexpr double ZeroVelocity_scale{1.0e2};
 constexpr double TargetPosition_scale{1.0e2};
-constexpr double TM_AboveBox_scale{1.0e1};
 constexpr double SensorAimAtObjectCenter_scale{1.0e2};
 constexpr double SensorAlignsWithPivot_scale{1.0e2};
 constexpr double SensorDistanceToObject_scale{5.0e1};
@@ -139,17 +140,19 @@ void groundTreePickUp(const mp::Interval& it, const mp::TreeBuilder& tb, const s
 
 void groundTreeUnStack(const mp::Interval& it, const mp::TreeBuilder& tb, const std::vector<std::string>& facts, KOMO_ext* komo, int verbose)
 {
-//  mp::Interval all{{it.time.from, it.time.to - 0.01}, it.edge}; // needs to revise ors to update limits whoch are not conistent with scaling?
-//  if(activateObjectives)  W(komo).addObjective(all, tb, new LimitsConstraint(0.05), OT_ineq, NoArr, 1.0, 0);
+  mp::Interval all{{it.time.from, it.time.to - 0.01}, it.edge};
+  if(activateObjectives)  W(komo).addObjective(all, tb, new mp::AgentKinBounds(), OT_ineq, NoArr, JointBounds_scale, 0);
 
   const auto& eff = "franka_hand";
   const auto& object = facts[0].c_str();
   const bool flipped = (facts[2] == "TRUE");
 
-  // approach
-  mp::Interval before{{it.time.to - 0.3, it.time.to - 0.3}, it.edge};
-  const auto approach_position = flipped ? ARR( -0.1, 0.0, 0.0 ) : ARR( 0.0, 0.0, 0.1 );
-  if(activateObjectives) W(komo).addObjective( before, tb, new TargetPosition( eff, object, approach_position ), OT_sos, NoArr, TargetPosition_scale, 0 ); // coming from above
+  // never go under the table
+  if(activateObjectives) W(komo).addObjective( all, tb, new TargetZPosition( eff, "tableC", 0.05, -1.0 ), OT_ineq, NoArr, 1e1, 0 ); // never go under the table
+
+  // approach coming from above
+  mp::Interval before{{it.time.from + 0.35, it.time.to - 0.35}, it.edge};
+  if(activateObjectives) W(komo).addObjective( before, tb, new TargetZPosition( eff, "tableC", 0.125, -1.0 ), OT_ineq, NoArr, 1e1, 0 ); // come from above
 
   // switch
   mp::Interval st{{it.time.to, it.time.to}, it.edge};
@@ -161,7 +164,7 @@ void groundTreeUnStack(const mp::Interval& it, const mp::TreeBuilder& tb, const 
   mp::Interval end{{it.time.to, it.time.to}, it.edge};
   if(komo->k_order > 1)
   {
-    mp::Interval just_after{{it.time.to, it.time.to + 0.2}, it.edge};
+    mp::Interval just_after{{it.time.to, it.time.to + 0.15}, it.edge};
     if(activateObjectives) W(komo).addObjective( just_after, tb, new ZeroVelocity( object ), OT_eq, NoArr, ZeroVelocity_scale, 1 ); // force the object not to move when starting to pick (mainly to force it not to go under the table!) MAYBE completely wrong?! does it really affect velocities?
   }
 
@@ -173,19 +176,19 @@ void groundTreeUnStack(const mp::Interval& it, const mp::TreeBuilder& tb, const 
 
 void groundTreePutDown(const mp::Interval& it, const mp::TreeBuilder& tb, const std::vector<std::string>& facts, KOMO_ext* komo, int verbose)
 {
-//  mp::Interval all{{it.time.from, it.time.to - 0.01}, it.edge};
-//  if(activateObjectives)  W(komo).addObjective(all, tb, new LimitsConstraint(0.05), OT_ineq, NoArr, 1.0, 0); // needs to revise ors to update limits whoch are not conistent with scaling?
+  mp::Interval all{{it.time.from, it.time.to - 0.01}, it.edge}; // needs to revise ors to update limits which are not conistent with scaling?
+  if(activateObjectives)  W(komo).addObjective(all, tb, new mp::AgentKinBounds(), OT_ineq, NoArr, JointBounds_scale, 0);
 
+  const auto& eff = "franka_hand";
   const auto& object = facts[0].c_str();
   const auto& place = facts[1].c_str();
 
-  mp::Interval before{{it.time.to - 0.3, it.time.to - 0.3}, it.edge};
+  // never go under the table
+  if(activateObjectives) W(komo).addObjective( all, tb, new TargetZPosition( eff, place, 0.05, -1.0 ), OT_ineq, NoArr, 1e1, 0 ); // never go under the table
 
-  // approach
-  if(activateObjectives) W(komo).addObjective( before, tb, new TargetPosition( object, place, ARR( 0.0, 0.0, 0.1 ) ), OT_sos, NoArr, TargetPosition_scale, 0 ); // coming from above
-
-  mp::Interval end{{it.time.to, it.time.to}, it.edge};
-  if(activateObjectives) W(komo).addObjective(end, tb, new TM_AboveBox(komo->world, object, place), OT_ineq, NoArr, TM_AboveBox_scale, 0);
+  // approach coming from above
+  mp::Interval before{{it.time.from + 0.35, it.time.to - 0.35}, it.edge};
+  if(activateObjectives) W(komo).addObjective( before, tb, new TargetZPosition( eff, place, 0.1, -1.0 ), OT_ineq, NoArr, 1e1, 0 ); // come from above
 
   // switch
   mp::Interval st{{it.time.to, it.time.to}, it.edge};
@@ -196,8 +199,8 @@ void groundTreePutDown(const mp::Interval& it, const mp::TreeBuilder& tb, const 
 
   if(komo->k_order > 1)
   {
-    mp::Interval just_after{{it.time.to, it.time.to + 0.2}, it.edge};
-    if(activateObjectives) W(komo).addObjective( just_after, tb, new ZeroVelocity( object ), OT_eq, NoArr, ZeroVelocity_scale, 1 ); // force the object not to move when starting to pick (mainly to force it not to go under the table)
+    //mp::Interval just_after{{it.time.to - 0.1, it.time.to - 0.01}, it.edge};
+    //if(activateObjectives) W(komo).addObjective( just_after, tb, new ZeroVelocity( object ), OT_eq, NoArr, ZeroVelocity_scale, 1 ); // force the object not to move when starting to pick (mainly to force it not to go under the table)
   }
 
   if(verbose > 0)
@@ -214,11 +217,13 @@ void groundTreePutDownFlipped(const mp::Interval& it, const mp::TreeBuilder& tb,
 
 void groundTreeCheck(const mp::Interval& it, const mp::TreeBuilder& tb, const std::vector<std::string>& facts, KOMO_ext* komo, int verbose)
 {
-//  mp::Interval all{{it.time.from, it.time.to - 0.01}, it.edge};
-//  if(activateObjectives)  W(komo).addObjective(all, tb, new LimitsConstraint(0.05), OT_ineq, NoArr, 1.0, 0); // needs to revise ors to update limits whoch are not conistent with scaling?
+  mp::Interval all{{it.time.from, it.time.to - 0.01}, it.edge}; // needs to revise ors to update limits which are not conistent with scaling?
+  if(activateObjectives)  W(komo).addObjective(all, tb, new mp::AgentKinBounds(), OT_ineq, NoArr, JointBounds_scale, 0);
+  //
 
   const auto& eff = "franka_hand";
   const auto& object = facts[0].c_str();
+  const auto& side = facts[1].c_str();
 
   std::map<std::string, arr> sideToPivot{
     {"side_0", ARR( -0.05, 0.0, 0.0 )},
@@ -229,9 +234,12 @@ void groundTreeCheck(const mp::Interval& it, const mp::TreeBuilder& tb, const st
     {"side_5", ARR( 0.00, 0.00, -0.05 )}
   };
 
+  // never go under the table
+  if(activateObjectives) W(komo).addObjective( all, tb, new TargetZPosition( eff, object, 0.03, -1.0 ), OT_ineq, NoArr, 1e1, 0 );
+
   mp::Interval end{{it.time.to - 0.2, it.time.to}, it.edge};
   if(activateObjectives) W(komo, verbose).addObjective(end, tb, new SensorAimAtObjectCenter( eff, object, ARR( 0, 0, -1 ) ), OT_eq, NoArr, SensorAimAtObjectCenter_scale, 0 );
-  if(activateObjectives) W(komo, verbose).addObjective(end, tb, new SensorAlignsWithPivot( eff, object, sideToPivot[facts[1]], 45.0 * 3.1415 / 180.0 ), OT_ineq, NoArr, SensorAlignsWithPivot_scale, 0 );
+  if(activateObjectives) W(komo, verbose).addObjective(end, tb, new SensorAlignsWithPivot( eff, object, sideToPivot[side], 45.0 * 3.1415 / 180.0 ), OT_ineq, NoArr, SensorAlignsWithPivot_scale, 0 );
   //if(activateObjectives) W(komo).addObjective(end, tb, new SensorAlignsWithPivot( eff, object, sideToPivot[facts[1]], 45.0 * 3.1415 / 180.0 ), OT_sos, NoArr, 5e1, 0 );
   if(activateObjectives) W(komo, verbose).addObjective(end, tb, new SensorDistanceToObject( eff, object, 0.2, 0.0 ), OT_sos, NoArr, SensorDistanceToObject_scale, 0 );
 
--- apps/lgp-tree-po-blocks-explo-franka/main.cpp
+++ apps/lgp-tree-po-blocks-explo-franka/main.cpp
@@ -20,6 +20,7 @@
 #include <approx_shape_to_sphere.h>
 #include <observation_tasks.h>
 #include <object_manipulation_tamp_controller.h>
+#include <utility.h>
 
 #include "komo_tree_groundings.h"
 
@@ -83,17 +84,15 @@ void display_robot()
 //    rai::wait( 300, true );
 }
 
-void plan_1_block()
+void plan_1_block(const double r0)
 {
-  srand(1);
+  //srand(1);
 
   // build planner
   matp::MCTSPlanner tp;
   mp::KOMOPlanner mp;
 
-  tp.setR0( -5.0, 1.50 ); // former setup working well: -0.5, 15.0
-  tp.setNIterMinMax( 50000, 1000000 );
-  tp.setRollOutMaxSteps( 50 );
+
   tp.setNumberRollOutPerSimulation( 1 );
   tp.setVerbose( false );
 
@@ -101,10 +100,17 @@ void plan_1_block()
   mp.setMinMarkovianCost( 0.00 );
   mp.setMaxConstraint( 15.0 );
   mp.addCostIrrelevantTask( "SensorDistanceToObject" );
+  mp.addCostIrrelevantTask( "FixSwichedObjects" );
 
   // set problem
-  tp.setFol( "LGP-1-block-6-sides-fol.g" );
-  mp.setKin( "LGP-1-block-6-sides-kin.g" );
+  // A
+  {
+    tp.setR0( r0, 15.0 ); // -1.0, -10.0
+    tp.setNIterMinMax( 100000, 1000000 );
+    tp.setRollOutMaxSteps( 50 );
+    tp.setFol( "LGP-1-block-6-sides-fol.g" );
+    mp.setKin( "LGP-1-block-6-sides-kin.g" );
+  }
 
   // register symbols
   mp.registerInit( groundTreeInit );
@@ -119,20 +125,92 @@ void plan_1_block()
   ObjectManipulationTAMPController tamp(tp, mp);
   TAMPlanningConfiguration config;
   config.watchMarkovianOptimizationResults = false;
-  config.watchJointOptimizationResults = true;
+  config.watchJointOptimizationResults = false;
+
+  std::ofstream params("results/params.data");
+  params << "c0: " << -r0 << std::endl;
+  params.close();
+
   tamp.plan(config);
 }
 
 void plan_2_blocks()
 {
-  srand(1);
+  // debug task planning
+//  {
+//    matp::MCTSPlanner tp;
+
+//    tp.setR0( -1.0, 1.0 );
+//    tp.setNIterMinMax( 500, 100000 );
+//    tp.setRollOutMaxSteps( 50 );
+//    tp.setNumberRollOutPerSimulation( 1 );
+//    tp.setVerbose( false );
+
+//    tp.setFol( "LGP-2-blocks-6-sides-fol.g" );
+
+//    tp.solve();
+//    auto policy = tp.getPolicy();
+//    savePolicyToFile( policy, "-candidate" );
+//  }
+
+  // debug motion planning
+//  {
+//    Policy policy;
+//    policy.load("policy-0-candidate");
+
+//    mp::KOMOPlanner mp;
+//    mp.setNSteps( 20 );
+//    mp.setMinMarkovianCost( 0.00 );
+//    mp.setMaxConstraint( 15.0 );
+//    mp.addCostIrrelevantTask( "FixSwichedObjects" );
+//    mp.addCostIrrelevantTask( "SensorDistanceToObject" );
+//    mp.addCostIrrelevantTask( "SensorAlignsWithPivot" );
+//    mp.addCostIrrelevantTask( "SensorAimAtObjectCenter" );
+//    mp.addCostIrrelevantTask( "ZeroVelocity" );
+
+//    mp.setKin( "LGP-2-block-6-sides-kin.g" );
+
+//    mp.registerInit( groundTreeInit );
+//    mp.registerTask( "pick-up"      , groundTreePickUp );
+//    mp.registerTask( "put-down"     , groundTreePutDown );
+//    mp.registerTask( "put-down-flipped", groundTreePutDownFlipped );
+//    mp.registerTask( "check"        , groundTreeCheck );
+//    mp.registerTask( "stack"        , groundTreeStack );
+//    mp.registerTask( "stack-flipped", groundTreeStack );
+//    mp.registerTask( "unstack"      , groundTreeUnStack );
+
+//    {
+//      auto po     = MotionPlanningParameters( policy.id() );
+//      po.setParam( "type", "markovJointPath" );
+//      mp.solveAndInform( po, policy );
+//    }
+//    savePolicyToFile( policy, "-informed" );
+
+//    { // evaluate and display markovian -> output in optimizationReportMarkovianPathTree.re
+//      auto po     = MotionPlanningParameters( policy.id() );
+//      po.setParam( "type", "EvaluateMarkovianCosts" );
+//      mp.solveAndInform( po, policy, false );
+//    }
+
+//    /// DECOMPOSED SPARSE OPTIMIZATION
+//    // adsm
+//    {
+//      auto po     = MotionPlanningParameters( policy.id() );
+//      po.setParam( "type", "ADMMCompressed" ); //ADMMSparse, ADMMCompressed
+//      po.setParam( "decompositionStrategy", "Identity" ); // SubTreesAfterFirstBranching, BranchGen, Identity
+//      po.setParam( "nJobs", "8" );
+//      mp.solveAndInform( po, policy, true );   // optimize and displays joint optimization -> output in optimizationReportAdmmCompressed.re
+//    }
+//  }
+  //
+  //srand(1);
 
   // build planner
   matp::MCTSPlanner tp;
   mp::KOMOPlanner mp;
 
   tp.setR0( -1.0, 1.0 );
-  tp.setNIterMinMax( 500, 100000 );
+  tp.setNIterMinMax( 5000, 100000 );
   tp.setRollOutMaxSteps( 50 );
   tp.setNumberRollOutPerSimulation( 1 );
   tp.setVerbose( false );
@@ -140,7 +218,14 @@ void plan_2_blocks()
   mp.setNSteps( 20 );
   mp.setMinMarkovianCost( 0.00 );
   mp.setMaxConstraint( 15.0 );
+  mp.addCostIrrelevantTask( "FixSwichedObjects" );
   mp.addCostIrrelevantTask( "SensorDistanceToObject" );
+  mp.addCostIrrelevantTask( "SensorAlignsWithPivot" );
+  mp.addCostIrrelevantTask( "SensorAimAtObjectCenter" );
+  mp.addCostIrrelevantTask( "ZeroVelocity" );
+  mp.addCostIrrelevantTask( "AgentKinBounds" );
+  mp.addCostIrrelevantTask( "TargetZPosition" );
+
 
   // set problem
   tp.setFol( "LGP-2-blocks-6-sides-fol.g" );
@@ -153,6 +238,7 @@ void plan_2_blocks()
   mp.registerTask( "put-down-flipped", groundTreePutDownFlipped );
   mp.registerTask( "check"        , groundTreeCheck );
   mp.registerTask( "stack"        , groundTreeStack );
+  mp.registerTask( "stack-flipped", groundTreeStack );
   mp.registerTask( "unstack"      , groundTreeUnStack );
 
   // build and run tamp controller
@@ -198,14 +284,34 @@ void planMCTS()
 
 //===========================================================================
 
+
+
 int main(int argc,char **argv)
 {
   rai::initCmdLine(argc,argv);
 
+  auto args = parseArguments(argc, argv);
+
   rnd.clockSeed();
 
-  //plan_1_block();
-  plan_2_blocks();
+  for (const auto& [key, value] : args)
+  {
+     std::cout << key << ": " << value << std::endl;
+  }
+
+  const uint n_blocks = std::stoi(value_or(args, "-n", "1"));
+
+  if(n_blocks == 1)
+  {
+    const double c0 = std::stof(value_or(args, "-c0", "5.0"));
+
+    plan_1_block(-c0); // algorithm requires r0
+  }
+
+  if(n_blocks == 2)
+  {
+    plan_2_blocks();
+  }
 
   //display_robot();
 
